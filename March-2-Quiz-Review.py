b2501da839ed335adf0f9ec874e01df222792efb
ZaireRiddick <zaire.riddick@ms223.org>
2016-05-03 09:16:32 -0400

    Create March-2-Quiz-Review.py

---
+++ March-2-Quiz-Review.py
@@ -0,0 +1,22 @@
+def compare_lists (list1, list2):
+list1 = [4, 5, 15, 11, 23, 42]
+list2 = [1, 8, 7, 16, 7, 35]
+if list1 == list2
+    for x in list
+        compare_lists(list1, list 2)
+
+def max_value(integer):
+x = 0
+while x < 50 + 1:
+    print x
+    x = x + 1
+
+
+    max_value(25)
+
+width = 6
+height = 6
+print "*" * width
+print "-" * width
+for x in range(height,0,-1):
+    print "*" * width
