c83406528cbc4eed1f57b700dfabe372245b6ecb
ZaireRiddick <zaire.riddick@ms223.org>
2016-05-06 08:38:21 -0400

    Create getting_loopy.py

---
+++ getting_loopy.py
@@ -0,0 +1,11 @@
+def fruit_pluralizer (list):
+    list [0] = "Orange" [:6] + "s"
+    for x in range(0,5):
+        list[x] = list[x]
+        list[x] =
+        #Making the code loop for an S or Ies
+
+list = ['Orange', 'Banana', 'Apple', 'Strawberry', 'Blueberry']
+print ("Single Fruit:" + str(list))
+fruit_pluralizer(list)
+print ("No longer single fruit" + str(list))
